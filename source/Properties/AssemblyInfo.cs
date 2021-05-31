232fff7a72e78130d0ee47681bfc3478c4140b1b
Nic Jansma <nic@nicj.net>
2021-05-31 19:41:12 -0400

    v1.6.0 binary

--- source/Properties/AssemblyInfo.cs
+++ source/Properties/AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TodoistBackup")]
-[assembly: AssemblyCopyright("Copyright © Nic Jansma 2014")]
+[assembly: AssemblyCopyright("Copyright © Nic Jansma 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
+[assembly: AssemblyVersion("1.6.0.0")]
+[assembly: AssemblyFileVersion("1.6.0.0")]
 
 [assembly: CLSCompliant(true)]
